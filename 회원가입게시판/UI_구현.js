adec290472aaaa4eaad7b93b4cf8c4b6bbb4ab72
genieaitch <hah4815@naver.com>
2024-11-12 09:34:36 +0900

    분기문 반복문 매개변수, 전달인자

---
+++ 회원가입게시판/UI_구현.js
@@ -0,0 +1,20 @@
+
+function showHint(){
+    const hint = document.getElementById("hint1")
+    hint.style.display = "block";
+}   
+
+function focusHint(){
+    const hint = document.getElementById("hint1")
+    hint.style.display = "none"
+}
+
+function showHint2(){
+    const hint = document.getElementById("hint2")
+    hint.style.display = "block";
+}   
+
+function focusHint2(){
+    const hint = document.getElementById("hint2")
+    hint.style.display = "none"
+}
